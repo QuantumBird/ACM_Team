58a03aca9ba379fbfe1611d72d6d71c49700ab08
QuantumBird <13707616187@163.com>
2019-08-12 18:00:13 +0800

    foo

--- gjz/String/T.cpp
+++ gjz/String/T.cpp
@@ -2,39 +2,15 @@
 #include <string>
 #include <cstring>
 using namespace std;
-const int maxn = 1e5 + 1;
-int node[maxn][11];
-int nptr = 1;
-bool insert(string & s){
-    int p = 1;
-    int ans = false;
-    for(int i = 0; i < s.length(); i++){
-        int ch = s[i] - '0';
-        if(node[p][10] == 1) return false;
-        if(node[p][ch] == 0){
-            node[p][ch] = ++nptr;
-            ans = true;
-        }
-        p = node[p][ch];
-    }
-    node[p][10] = 1;
-    return ans;
+const int maxn = 1e6 + 1;
+int tire[maxn][10];
+int nptr;
+void init() {
+    memset(tire, 0, sizeof(tire));
+    nptr = 1;
 }
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        string s;
-        bool ans = false;
-        nptr = 1;
-        memset(node, 0, sizeof(node));
-        for(int i = 0; i < n; i ++){
-            cin>>s;
-            ans = insert(s);
-        }
-        cout<<(ans? "YES": "NO")<<endl;
-    }
+bool insert(string & s) {
+
+int main() {
     return 0;
 }
