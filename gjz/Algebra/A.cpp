cb938ccd8a83ae711318ef97217e939e160e9476
QuantumBird <13707616187@163.com>
2019-08-20 02:50:43 +0800

    foo

--- gjz/Algebra/A.cpp
+++ gjz/Algebra/A.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <cstring>
+#include <climits>
 using namespace std;
 typedef unsigned long long ll;
 namespace matrix{
     const int maxn = 5;
     struct Mat{
-        int m[maxn][maxn];
+        ll m[maxn][maxn];
         Mat(){}
     };
     Mat zeros(){
@@ -20,6 +21,7 @@ namespace matrix{
         for(int i = 0; i < maxn; i ++)
             for(int j = 0; j < maxn; j ++)
                 ret.m[i][j] = (i == j? 1: 0);
+        return ret;
     }
     Mat operator*(const Mat & a, const Mat & b){
         Mat ans = zeros();
@@ -40,4 +42,33 @@ namespace matrix{
     }
 };
 int main(){
+    int T, kase = 0;
+    cin>>T;
+    while(T --){
+        ll p, q, n;
+        cin>>p>>q>>n;
+        ll tmp = ULLONG_MAX - q + 1;
+        matrix::Mat cst, mit;
+        mit = matrix::zeros();
+        mit.m[1][1] = p;
+        mit.m[1][2] = p*p + 2*tmp;
+        mit.m[1][3] = q;
+        cout<<"Case "<<++kase<<": ";
+        if(n == 0)
+            cout<<2<<endl;
+        else if(n == 1)
+            cout<<p<<endl;
+        else if(n == 2)
+            cout<<(p*p + 2*tmp)<<endl;
+        else{
+            cst = matrix::zeros();
+            cst.m[1][1] = 0;
+            cst.m[2][1] = 1;
+            cst.m[1][2] = tmp;
+            cst.m[2][2] = p;
+            cout<<(mit*(cst^(n - 2))).m[1][2]<<endl;
+        }
+    }
+    return 0;
+}
 
---
+++ gjz/Algebra/test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+int main(){
+    cout<<ULLONG_MAX<<endl;
+    return 0;
+}
+
