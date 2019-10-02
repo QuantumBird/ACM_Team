87490f916aaa164659efcff38d83e95a0acb2411
zhaolonga <2339824768@qq.com>
2019-10-02 15:51:04 +0800

    补题

--- LZL/codeforces/590/D.cpp
+++ LZL/codeforces/590/D.cpp
@@ -1,18 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int book[30];
-int sum;
+int q,m,n,l,r;
+char ch;
+set<int>se[26];
 int main(){
     string str;
     cin >> str;
-    size_t len = str.length();
+    int len = str.length();
     for(int i=0;i<len;++i){
-        if(book[str[i]-'0']==0){
-            book[str[i]-'0'] = 1;
-            ++sum;
+        se[str[i]-'a'].insert(i+1);
+    }
+    cin >> q;
+    while(q--){
+        cin >> m;
+        if(m==1){
+            cin >> n >> ch;
+            se[str[n-1]-'a'].erase(n);
+            str[n-1] = ch;
+            se[ch-'a'].insert(n);
+        }else{
+            int ans = 0;
+            cin >> l >> r;
+            for(int i=0;i<26;++i){
+                for(int j=l;j<=r;++j){
+                    if(se[i].find(j)!=se[i].end()){
+                        ++ans;
+                        break;
+                    }
+                }
+            }
+            cout << ans << endl;
         }
     }
-    cout << sum << endl;
     return 0;
 }
